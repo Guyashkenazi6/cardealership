7cc7d5934f9d8f678b03888d043358d258937056
Guyashkenazi6 <109524538+Guyashkenazi6@users.noreply.github.com>
2023-08-29 09:25:51 +0300

    Add files via upload

---
+++ main/main.py
@@ -0,0 +1,120 @@
+from fastapi import FastAPI
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+import requests
+
+# MongoDB setup
+# client = MongoClient("mongodb://localhost:27017")
+client = MongoClient("mongodb://172.17.0.1:27017")
+db_vehicles = client["vehiecls"]
+collection = db_vehicles["vehicels"]
+
+app = FastAPI()
+
+@app.get("/")
+async def home():
+    return "Hello, this is the Vehicle Management API!"
+
+@app.get("/vehicles", tags=["vehicles"])
+async def get_all_vehicles():
+    vehicle_data = []
+    for doc in collection.find():
+        vehicles = doc.get("vehicles", [])
+        for vehicle in vehicles:
+            vehicle_data.append(vehicle)
+    return {"data": vehicle_data}
+
+@app.get("/vehicles/{vehicle_id}", tags=["vehicles"])
+async def get_vehicle_by_id(vehicle_id: int):
+    vehicle = collection.find_one({"vehicles.vehicle_id": vehicle_id})
+    if vehicle is None:
+        return {"message": "Vehicle not found"}
+    
+    for veh in vehicle["vehicles"]:
+        if veh["vehicle_id"] == vehicle_id:
+            return veh
+    
+    return {"message": "Vehicle not found"}
+
+
+# "vehicle_id": collection.count_documents({}) + 1,  # Assign a new vehicle_id
+
+@app.post("/vehicles", tags=["vehicles"])
+async def create_vehicle(vehicle_id :int ,manufacturer: str, model: str, year: int, color: str, vehicle_price: float):
+    new_vehicle = {
+        "vehicle_id": vehicle_id,
+        "manufacturer": manufacturer,
+        "model": model,
+        "year": year,
+        "color": color,
+        "vehicle_price": vehicle_price
+    }
+    result = collection.insert_one({"vehicles": [new_vehicle]})
+    return {"message": "Vehicle created successfully", "vehicle_id": str(result.inserted_id)}
+
+@app.put("/vehicles/{vehicle_id}", tags=["vehicles"])
+async def update_vehicle(vehicle_id: int, manufacturer: str, model: str, year: int, color: str, vehicle_price: float):
+    updated_vehicle = {
+        "vehicle_id": vehicle_id,
+        "manufacturer": manufacturer,
+        "model": model,
+        "year": year,
+        "color": color,
+        "vehicle_price": vehicle_price
+    }
+    result = collection.update_one(
+        {"vehicles.vehicle_id": vehicle_id},
+        {"$set": {"vehicles.$": updated_vehicle}}
+    )
+    if result.modified_count > 0:
+        return {"message": "Vehicle updated successfully"}
+    else:
+        return {"message": "Vehicle not found"}
+
+@app.delete("/vehicles/{vehicle_id}", tags=["vehicles"])
+async def delete_vehicle(vehicle_id: int):
+    result = collection.update_one(
+        {"vehicles.vehicle_id": vehicle_id},
+        {"$pull": {"vehicles": {"vehicle_id": vehicle_id}}}
+    )
+    if result.modified_count > 0:
+        return {"message": "Vehicle deleted successfully"}
+    else:
+        return {"message": "Vehicle not found"}
+
+
+url_visa = "http://172.17.0.1:9010/receive"  
+
+@app.post("/order", tags=["order"])
+async def buy_car(customer: str, card_number: str, vehicle_id: int):
+
+    vehicle_data = collection.find_one({"vehicles.vehicle_id": vehicle_id})
+    if vehicle_data is None:
+        return {"message": "Vehicle not found"}
+
+    selected_vehicle = None
+    for vehicle in vehicle_data["vehicles"]:
+        if vehicle["vehicle_id"] == vehicle_id:
+            selected_vehicle = vehicle
+            break
+
+    if selected_vehicle is None:
+        return {"message": "Vehicle not found"}
+
+    data_to_send = {
+        "customer": customer,
+        "card_number": card_number,
+        "amount": selected_vehicle["vehicle_price"],
+        "vehicle_id": vehicle_id
+    }
+
+    print("Sending data:", data_to_send)
+
+    res = requests.post(url=url_visa, json=data_to_send)  # Sending to visa service
+    print("Response:", res.text)
+
+    return res.json()
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=9000)
---
+++ visa/visa.py
@@ -0,0 +1,169 @@
+from fastapi import FastAPI
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+import uvicorn
+# import json
+import random
+# from pydantic import BaseModel
+# import string
+
+# class Visa(BaseModel):
+#     customer : str
+#     card_number : str
+#     amount : str
+
+app = FastAPI()
+
+# def generate_transaction_id():
+#     return ''.join(random.choices(string.ascii_uppercase + string.digits, k=10))
+
+# MongoDB setup
+# client = MongoClient("mongodb://localhost:27017")
+client = MongoClient("mongodb://172.17.0.1:27017")
+db = client["visa"]
+received_data_collection = db["visa"]  # Use the existing "visa" collection
+success_visa_collection = db["success"]
+rejected_visa_collection = db["rejected"]
+
+
+
+@app.post("/receive", tags=["receive"])
+async def receive_post_request(data: dict):
+    # Process the received data
+    print(data)
+
+    transection_id = random.randint(10000, 99999)
+    statuses = ["success"] * 8 + ["failure"] * 2
+    random_status = random.choice(statuses)
+
+    if random_status == "success":
+        transaction_data_good = {
+            "customer_id": data["customer"],
+            "card_number": data["card_number"],
+            "amount": data["amount"],
+            "transaction_id": transection_id,
+            "status": random_status
+        }
+        success_visa_collection.insert_one(transaction_data_good)
+        
+        
+
+
+        return {
+            "message": "Transaction successful",
+            "customer_id": data["customer"],
+            "transaction_id": transection_id,
+            "status": random_status
+        }
+    
+    elif random_status == "failure":
+        transaction_data_bad = {
+            "customer_id": data["customer"],
+            "card_number": data["card_number"],
+            "amount": data["amount"],
+            "transaction_id": transection_id,
+            "status": random_status
+        }
+        rejected_visa_collection.insert_one(transaction_data_bad)
+    
+        
+
+        return {
+            "message": "Transaction failed",
+            "customer_id": data["customer"],
+            "transaction_id": transection_id,
+            "status": random_status
+        }
+    
+    # received_data_collection.insert_one({"data": data})
+
+    return {"message": "Data received successfully"}
+
+# @app.post("/Transection", tags=["Transection"])
+# async def Transection():
+#     received_data = received_data_collection.find_one({})["data"]
+#     # received_data = received_data_collection.find_one({})
+
+#     vehicle_id = received_data["vehicle_id"]
+
+#     db_vehicles_client = MongoClient("mongodb://172.17.0.1:27017")
+#     db_vehicles = db_vehicles_client["vehiecls"]
+#     vehicle_data = db_vehicles["vehicels"].find_one({"vehicles.vehicle_id": vehicle_id})
+
+
+
+#     vehicle_data = db_vehicles["vehicels"].find_one({"vehicles.vehicle_id": vehicle_id})
+#     if vehicle_data is None:
+#         return {"message": "Vehicle not found"}
+
+#     selected_vehicle = None
+#     for vehicle in vehicle_data["vehicles"]:
+#         if vehicle["vehicle_id"] == vehicle_id:
+#             selected_vehicle = vehicle
+#             break
+
+#     if selected_vehicle is None:
+#         return {"message": "Vehicle not found"}
+
+#     transection_id = random.randint(10000, 99999)
+
+#     if random_status == "success":
+#         transaction_data_good = {
+#             "customer_id": received_data["customer"],
+#             "card_number": received_data["card_number"],
+#             "amount": selected_vehicle["vehicle_price"],
+#             "transaction_id": transection_id,
+#             "status": random_status
+#         }
+#         success_visa_collection.insert_one(transaction_data_good)
+        
+        
+
+
+#         return {
+#             "message": "Transaction successful",
+#             "customer_id": received_data["customer"],
+#             "transaction_id": transection_id,
+#             "status": random_status
+#         }
+#     elif random_status == "failure":
+#         transaction_data_bad = {
+#             "customer_id": received_data["customer"],
+#             "card_number": received_data["card_number"],
+#             "amount": selected_vehicle["vehicle_price"],
+#             "transaction_id": transection_id,
+#             "status": random_status
+#         }
+#         rejected_visa_collection.insert_one(transaction_data_bad)
+    
+        
+
+#         return {
+#             "message": "Transaction failed",
+#             "customer_id": received_data["customer"],
+#             "transaction_id": transection_id,
+#             "status": random_status
+#         }
+
+
+@app.get("/")
+async def home():    
+    return "hello" 
+
+
+@app.get("/Good",tags = ["Good"])
+async def get_all_good():
+    my_good_json = list(success_visa_collection.find({}))
+    for data in my_good_json:
+        data["_id"] = str(data["_id"])
+    return my_good_json
+
+@app.get("/Bad",tags = ["Bad"])
+async def get_all_bad():
+    my_bad_json = list(rejected_visa_collection.find({}))
+    for data in my_bad_json:
+        data["_id"] = str(data["_id"])
+    return my_bad_json
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=9010)
